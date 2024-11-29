58571d94bf194d7bf662e1d722aa3b7ec8d22ae9
ericFlutterast <erikGitFlutter@gmail.com>
2024-11-29 18:29:55 +0300

    Initial Commit

---
+++ swift_data_struckts/swift_data_strucktsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_data_strucktsApp.swift
+//  swift_data_struckts
+//
+//  Created by ericFlutter on 29.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_data_strucktsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
